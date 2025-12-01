af71f1da163605f78478679d612287e8f7ac3a41
iamGabriel08 <gabriel.schaehle@gmail.com>
2025-12-01 08:15:24 +0100

    Klasse LoadCell hinzugefügt

--- src/LoadCell.cpp
+++ src/LoadCell.cpp
@@ -1,1 +1,14 @@
+#include "LoadCell.h"
 
+
+//========== Konstruktor ==========//   
+LoadCell::LoadCell(const uint8_t dataPin, const uint8_t clockPin): _dataPin(dataPin), _clockPin(clockPin) {
+    _scale.begin(_dataPin, _clockPin);
+}
+
+//========== Öffentliche Funktions-Implementierungen  ==========//
+
+
+
+
+//========== Private Funktions-Implementierungen  ==========//
--- src/LoadCell.h
+++ src/LoadCell.h
@@ -1,6 +1,19 @@
 #ifndef LoadCell_h
 #define LoadCell_h
 #include <Arduino.h>
+#include "HX711.h"
+#include <stdint.h>
 
+class LoadCell{
+
+    public:
+        LoadCell(const uint8_t dataPin, const uint8_t clockPin);
+
+
+    private:
+        const uint8_t _dataPin;
+        const uint8_t _clockPin;
+        HX711 _scale;
+};
 
 #endif
